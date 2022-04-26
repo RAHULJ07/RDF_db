9b9af1a7bacc8897aba9e9067da8fe73aebdf895
Rahul Kumar <rahulrjjaiswal07@gmail.com>
2022-04-25 21:40:24 -0700

    fixed build

--- javaminibase/src/db/ExecutionStrategies/QueryExecutionStrategies.java
+++ javaminibase/src/db/ExecutionStrategies/QueryExecutionStrategies.java
@@ -268,7 +268,7 @@ public class QueryExecutionStrategies implements IQueryExecutionStrategies{
         executeSort();
       }
       Telemetry.endOperation("Sort");
-      Telemetry.prinTelemetry();
+      Telemetry.printTelemetry();
 
 
 
